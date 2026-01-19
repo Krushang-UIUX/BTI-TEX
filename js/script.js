658bb36e80a22e5b0d7458e12cbb3079a3bbfdf2
Krushang-UIUX <123441261+Krushang-Ordex@users.noreply.github.com>
2026-01-19 17:08:08 +0530

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+$(function(){
+    $(".xzoom, .xzoom-gallery").xzoom({
+        zoomWidth:400,
+        tint: "#333",
+        Xoffset: 15,
+    });
+
+    $(".xzoom1, .xzoom-gallery1").xzoom({
+        zoomWidth:400,
+        tint: "#333",
+        Xoffset: 15,
+    });
+
+    $(".xzoom2, .xzoom-gallery2").xzoom({
+        zoomWidth:400,
+        tint: "#333",
+        Xoffset: 15,
+    });
+
+    $(".xzoom3, .xzoom-gallery3").xzoom({
+        zoomWidth:400,
+        tint: "#333",
+        Xoffset: 15,
+    });
+
+    $(".xzoom4, .xzoom-gallery4").xzoom({
+        zoomWidth:400,
+        tint: "#333",
+        Xoffset: 15,
+    });
+
+    $(".xzoom5, .xzoom-gallery5").xzoom({
+        zoomWidth:400,
+        tint: "#333",
+        Xoffset: 15,
+    });
+
+    $(".xzoom6, .xzoom-gallery6").xzoom({
+        zoomWidth:400,
+        tint: "#333",
+        Xoffset: 15,
+    });
+});
